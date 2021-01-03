f1724d822eff9414a916d5d3aca94c78ce2bbac7
TomyPrs <tomy.prs25@gmail.com>
2021-01-03 19:38:58 +0700

    cleanup: uncoment noqa
    Signed-off-by: TomyPrs <tomy.prs25@gmail.com>

--- neko_bot/__init__.py
+++ neko_bot/__init__.py
@@ -1,5 +1,5 @@
 """Initialize Framework"""
 
-from .config import Config  # noqa: F401
-from .nekobot import neko  # noqa: F401
-from .core import cust_cmd # noqa: F401
+from .config import Config  # noqa
+from .nekobot import neko  # noqa
+from .core import cust_cmd # noqa
