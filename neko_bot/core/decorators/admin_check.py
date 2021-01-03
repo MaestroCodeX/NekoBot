764f198249d61fc36d2df60565b065f70de71f8e
Mr.Miss <keselekpermen@mrmiss.wtf>
2021-01-03 18:10:03 +0700

    handle more arguments

--- neko_bot/core/decorators/admin_check.py
+++ neko_bot/core/decorators/admin_check.py
@@ -17,11 +17,11 @@ async def adminlist(client, chat_id):
 def admin(coro):
     """This function build decorator for administ."""
     @wraps(coro)
-    async def decorator(client, message):
+    async def decorator(client, message, *args, **kwargs):
         chat_id = message.chat.id
-        administ = await adminlist(client, chat_id)
+        administ = await adminlist(client, chat_id, *args, **kwargs)
         if message.from_user.id in administ:
-            return await coro(client, message)
+            return await coro(client, message, *args, **kwargs)
         else:
             return await message.reply_text("You are not an admin!")
 
@@ -38,12 +38,12 @@ def staff(rank: str = "sudo"):
     def decorators(coro):
 
         @wraps(coro)
-        async def check(client, message):
+        async def check(client, message, *args, **kwargs):
             user_id = message.from_user.id
             if rank in ["owner", "sudo"] and user_id == Config.OWNER_ID:
-                return await coro(client, message)
+                return await coro(client, message, *args, **kwargs)
             elif rank == "sudo" and user_id in Config.SUDO_USERS:
-                return await coro(client, message)
+                return await coro(client, message, *args, **kwargs)
             else:
                 LOGGER.error("Unkown rank \"%s\"", rank)
                 return
